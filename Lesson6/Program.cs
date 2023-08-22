51ddeee98d37917c73c6cd0e68f23c7d67ff6900
Robertas Stankevičius <stankevic.robertas@gmail.com>
2023-08-22 19:00:17 +0300

    Added spaces between lines

---
+++ Lesson6/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+int age = 28;
+string name = "Robertas Stankevicius";
+string position = "Software Developer";
+string email = "Robertas.Stankevicius@example.com";
+string phone = "+370 111 11 111";
+
+Console.WriteLine("================ VIZITINE ===============");
+Console.WriteLine();
+Console.WriteLine("Vardas:     " + name);
+Console.WriteLine("Amzius:     " + age);
+Console.WriteLine("Pareigos:   " + position);
+Console.WriteLine("El. Pastas: " + email);
+Console.WriteLine("Tel.:       " + phone);
+Console.WriteLine();
+Console.WriteLine("=========================================");
